9e8952ffdcd25c48a08d6d7681cb103fb3ef74f1
Jiangmmh <minghanjiang@qq.com>
2025-09-15 10:08:00 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,11 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;        // 根据main函数中的使用，写成as fruit；为了让外部访问，use前加pub
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
